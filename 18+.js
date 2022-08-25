a87593e5d7100485e348c7b93dab4adbbb206796
git-pro-shivam <111572063+git-pro-shivam@users.noreply.github.com>
2022-08-25 18:56:54 +0530

    Add files via upload

---
+++ 18+.js
@@ -0,0 +1,10 @@
+let age = confirm("are you 18+")
+
+if(age){
+   alert("looking very exited");
+ }else{
+   alert("go and study <br>KID");
+ }
+ function hello(){
+  alert("are you 18+");
+}
---
+++ user experinces.js
@@ -0,0 +1,4 @@
+let ux = prompt("tell us yor exprinses about our website so we can improve it")
+if(age){
+alert("THANKS");
+}
